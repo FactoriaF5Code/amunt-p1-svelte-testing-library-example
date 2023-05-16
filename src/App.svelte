5f5aa423ac75464ed2ef3b7bd96d3d78f75e1851
Javi <javi@jsrois.net>
2023-05-16 15:27:00 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Tarjeta from "./lib/Tarjeta.svelte";
+
+</script>
+<main>
+  <Tarjeta word={"pepita"} />
+</main>
---
+++ src/lib/Tarjeta.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let word;
+
+    $: initial = word.at(0);
+    $: size = word.length;
+</script>
+
+<div class="card">
+    <h1>"{word}"</h1>
+    <p>tiene {size} letras</p>
+    <p>comienza por la letra {initial}</p>
+</div>
+
+<style>
+    div {
+        border: 1px dashed orangered;
+    }
+</style>
